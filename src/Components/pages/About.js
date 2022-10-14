cddbd63b8b4a468e5487f7a0214e725d40a8a7c9
Akshsay72 <78556321+Akshsay72@users.noreply.github.com>
2022-10-14 13:43:39 +0530

    Add files via upload

---
+++ src/Components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+        <h1>CRUD -> Applications and it's Manipulations by using ReactJS</h1>
+        <h4>
+          In this project, i have created an website in which table(database) is created and going wo manipulate on the table.
+          CRUD is basically a process though which we manipulate on the table. CRUD -> Create Read Use and Delete.
+          We get acces to use the table by using json-server. It will help you to work upon database, acts as a backend on the database.
+        </h4>
+    </div>
+  );
+};
+
+export default About;
